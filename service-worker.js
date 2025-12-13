c69d67139e313c4bc8ca00fede067cb012b36664
46memorybit <secretgiants819@gmail.com>
2025-12-13 23:08:41 +0900

    Refactor service worker cache management

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,21 @@
-const CACHE_NAME = 'random-query-pwa-v1';
-const FILES_TO_CACHE = [
-  '/Auto_Link/',
-  '/Auto_Link/index.html',
-  '/Auto_Link/app.js',
-  '/Auto_Link/db.js',
-  '/Auto_Link/manifest.webmanifest',
-  '/Auto_Link/icons/icon-192.png',
-  '/Auto_Link/icons/icon-512.png'
+const CACHE_NAME = 'auto-link-v1';
+const FILES = [
+'/auto_link/index.html',
+'/auto_link/app.js',
+'/auto_link/db.js',
+'/auto_link/manifest.webmanifest'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+);
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  self.clients.claim();
-});
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+e.respondWith(
+caches.match(e.request).then(res => res || fetch(e.request))
+);
 });
