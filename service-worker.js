03e1d703486453781d95696c4d1f6b5556a56ea3
46memorybit <secretgiants819@gmail.com>
2025-12-13 23:39:11 +0900

    Refactor service worker for cache naming and event handling

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,20 @@
-const CACHE = 'link-pwa-v1';
+const CACHE_NAME = 'auto-link-v1';
 const ASSETS = [
-'./',
-'./index.html',
-'./app.js',
-'./db.js',
-'./manifest.webmanifest'
+  './',
+  './index.html',
+  './app.js',
+  './db.js',
+  './manifest.webmanifest'
 ];
 
-
-
-
-self.addEventListener('install', e => {
-e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-
-
-
-self.addEventListener('fetch', e => {
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request))
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
