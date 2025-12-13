c442989d6c60891081c9888bc9bf4d6d8eedc74d
46memorybit <secretgiants819@gmail.com>
2025-12-13 23:34:27 +0900

    Update cache name in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'link-auto-v1';
+const CACHE = 'link-pwa-v1';
 const ASSETS = [
 './',
 './index.html',
@@ -8,13 +8,17 @@ const ASSETS = [
 ];
 
 
+
+
 self.addEventListener('install', e => {
 e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 
+
+
 self.addEventListener('fetch', e => {
 e.respondWith(
-caches.match(e.request).then(res => res || fetch(e.request))
+caches.match(e.request).then(r => r || fetch(e.request))
 );
 });
